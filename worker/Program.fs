fda7b024b209b6347a24e6b1623820e077cb0f5f
y2k <itwisterlx@gmail.com>
2017-07-12 21:44:34 +0300

    refactoring

--- worker/Program.fs
+++ worker/Program.fs
@@ -3,6 +3,23 @@ open EasyNetQ
 open Spectator.Core
 open Spectator.Worker
 
+let createNewSubscriptions (bus: IBus) = async {
+    let! resp = bus.RequestAsync<Command, Responses> GetNewSubscriptions |> Async.AwaitTask
+    let newSubs = match resp with | NewSubscriptions x -> x | _ -> []
+    let! xs = newSubs
+              |> List.map (fun x -> RssParser.IsValid x.uri)
+              |> Async.Parallel
+
+    let subs = newSubs |> List.zip (xs |> Array.toList)
+                       |> List.map (fun (isRss, x) -> match isRss with
+                                                      | true  -> (x.uri, Provider.Rss)
+                                                      | false -> (x.uri, Provider.Invalid))
+
+    do! CreateSubscriptions subs |> bus.PublishAsync |> Async.AwaitTask
+}
+
+let delay = Async.Sleep(10000)
+
 [<EntryPoint>]
 let main argv =
     // let doc = System.Xml.Linq.XDocument.Parse(System.IO.File.ReadAllText("examples/wiki_atom.xml"))
@@ -11,20 +28,9 @@ let main argv =
 
     let bus = RabbitHutch.CreateBus("host=localhost")
     let rec doWork () = async {
-        let! resp = bus.RequestAsync<Command, Responses> GetNewSubscriptions |> Async.AwaitTask
-        let newSubs = match resp with | NewSubscriptions x -> x | _ -> []
-        let! xs = newSubs
-                  |> List.map (fun x -> RssParser.IsValid x.uri)
-                  |> Async.Parallel
+        do! createNewSubscriptions bus
 
-        let subs = newSubs |> List.zip (xs |> Array.toList)
-                           |> List.map (fun (isRss, x) -> match isRss with
-                                                          | true  -> (x.uri, Provider.Rss)
-                                                          | false -> (x.uri, Provider.Invalid))
-
-        do! CreateSubscriptions subs |> bus.PublishAsync |> Async.AwaitTask
-
-        do! Async.Sleep(10000)
+        do! delay
         return! doWork ()
     }
 
