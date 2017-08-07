d84eb5146c16f82ac93b07f4f7c48efd8a5ab144
y2k <itwisterlx@gmail.com>
2017-08-07 13:06:46 +0300

    refactoring

--- bot/Program.fs
+++ bot/Program.fs
@@ -12,19 +12,19 @@ module Domain =
         | "add" :: url :: _ -> Add url
         | _ -> Unknown
 
-    let formatSubList (newSubs : NewSubscription list) (subs : Subscription list) = 
+    let subListToMessageResponse (newSubs : NewSubscription list) (subs : Subscription list) = 
         newSubs
         |> List.map (fun x -> x.uri)
         |> List.append (subs |> List.map (fun x -> x.uri))
         |> List.fold (fun s x -> sprintf "%O\n- %O" s x) "Your subscriptions: "
 
-    let handle (message: Bot.Message) = async {
+    let responseToMessage (message: Bot.Message) = async {
         use bus = RabbitHutch.CreateBus("host=localhost")
         match parse message.text with
         | Ls -> 
             let! resp = bus.RequestAsync<Command, Responses>(GetUserSubscriptions message.user) |> Async.AwaitTask
             match resp with
-            | UserSubscriptions(newSubs, subs) -> return formatSubList newSubs subs
+            | UserSubscriptions(newSubs, subs) -> return subListToMessageResponse newSubs subs
             | _ -> return "Error"
         | Add url -> 
             let! resp = AddNewSubscription (message.user, Uri url) 
@@ -33,16 +33,16 @@ module Domain =
         | Unknown -> return "Commands: ls, add <url>"
     }
 
-    let handle' token (x: Bot.Message) = 
+    let handle token (x: Bot.Message) = 
         async {
             printfn "message: %O" x.text
-            let! res = handle x
+            let! res = responseToMessage x
             Bot.sendToTelegramSingle token x.user (res) |> ignore
         } |> Async.Start
 
 [<EntryPoint>]
 let main argv =
-    Bot.listerForMessages argv.[0] |> RX.add (Domain.handle' argv.[0])
+    Bot.listerForMessages argv.[0] |> RX.add (Domain.handle argv.[0])
     printfn "Listening for updates..."
     Threading.Thread.Sleep(-1);
     0
