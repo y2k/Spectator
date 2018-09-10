c6acc4b80bf209b571dcd99dfc9e3f32bd1e67f9
y2k <itwisterlx@gmail.com>
2018-09-10 10:36:01 +0300

    add suave & swagger

---
+++ web/Program.fs
@@ -0,0 +1,23 @@
+﻿open Suave
+open Suave.Swagger.Rest
+open Suave.Swagger.Swagger
+open Suave.Swagger.FunnyDsl
+open Spectator.Core
+
+[<CLIMutable>]
+type TimeResult = { items : Snapshot [] }
+
+let snapshots = 
+    MODEL { items = [||] }
+
+let api = 
+    swagger {
+        for route in getting (simpleUrl "/snapshots" |> thenReturns snapshots) do
+            yield route |> addResponse 200 "Featured snapshots" (Some typeof<TimeResult>)
+            yield route |> supportsJsonAndXml
+    }
+
+[<EntryPoint>]
+let main _ = 
+    startWebServer defaultConfig api.App
+    0
