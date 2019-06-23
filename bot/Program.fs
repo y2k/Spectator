d51e8d12a3ece0434ba6e0f64b7b85ae6f13c050
y2k <itwisterlx@gmail.com>
2019-06-23 11:47:49 +0300

    add remove command

--- bot/Program.fs
+++ bot/Program.fs
@@ -1,20 +1,22 @@
 ﻿module Spectator.Bot.App
 
 module private Domain =
-    open System
-    open Spectator.Core
     open MongoDB.Bson
     open MongoDB.Bson.Serialization
+    open Spectator.Core
+    open System
 
     type private Cmd =
         | GetUserSubscriptionsCmd of UserId
         | UnknownCmd
         | AddNewSubscriptionCmd of UserId * Uri
+        | DeleteSubscriptionCmd of UserId * Uri
 
     let private parse (message : Bot.Message) =
         match String.split message.text ' ' with
         | "/ls" :: _ -> GetUserSubscriptionsCmd message.user
         | "/add" :: url :: _ -> AddNewSubscriptionCmd(message.user, Uri url)
+        | "/rm" :: url :: _ -> DeleteSubscriptionCmd(message.user, Uri url)
         | _ -> UnknownCmd
 
     let private subListToMessageResponse (newSubs : NewSubscription list) (subs : Subscription list) =
@@ -23,6 +25,11 @@ module private Domain =
         |> List.append (subs |> List.map (fun x -> string x.uri))
         |> List.fold (sprintf "%s\n- %s") "Your subscriptions: "
 
+    let private deleteSubs db userId uri =
+        { db with
+            newSubscriptions = db.newSubscriptions |> List.filter ^ fun x -> x.userId <> userId || x.uri <> uri
+            subscriptions = db.subscriptions |> List.filter ^ fun x -> x.userId <> userId || x.uri <> uri }
+
     let handle message (db : CoEffectDb) =
         match parse message with
         | GetUserSubscriptionsCmd userId ->
@@ -30,16 +37,18 @@ module private Domain =
             subListToMessageResponse
                 (db.newSubscriptions |> List.filter ^ fun x -> x.userId = userId)
                 (db.subscriptions |> List.filter ^ fun x -> x.userId = userId)
+        | DeleteSubscriptionCmd(userId, uri) ->
+            deleteSubs db userId uri, "Your subscription deleted"
         | AddNewSubscriptionCmd(userId, uri) ->
             let sub = { id = Guid.NewGuid(); userId = userId; uri = uri }
             { db with newSubscriptions = sub :: db.newSubscriptions },
             "Your subscription created"
         | UnknownCmd ->
-            db, "/ls - Show your subscriptions\n/add [url] - Add new subscription"
+            db, "/ls - Show your subscriptions\n/add [url] - Add new subscription\n/rm [url] - Add new subscription"
 
 open Spectator.Core
 open Spectator.Infrastructure
 
 let start db =
-    Bot.repl ^ fun msg -> 
+    Bot.repl ^ fun msg ->
         dbContext db (Domain.handle msg)
