5071d1411e0269aab78f3ff82f1101296d9402a1
y2k <itwisterlx@gmail.com>
2020-11-27 02:07:17 +0300

    update dockerfile

--- application/HealthCheck.fs
+++ application/HealthCheck.fs
@@ -8,36 +8,6 @@ type State =
     { healthCheckComplete: bool }
     static member Empty = { healthCheckComplete = false }
 
-let updatePing state =
-    function
-    | HealthCheckRequested -> { healthCheckComplete = true }
-    | NewSubscriptionCreated _
-    | SubscriptionCreated _
-    | SubscriptionRemoved _
-    | SnapshotCreated _ -> state
-
-type t = { ctx: HttpListenerContext }
-
-let startServer url =
-    let listener = new HttpListener()
-    listener.Prefixes.Add url
-    listener.Start()
-
-    async {
-        let! ctx = listener.GetContextAsync()
-        return { ctx = ctx }
-    }
-
-let sendText { ctx = ctx } (text: string) =
-    async {
-        let bytes = Encoding.UTF8.GetBytes text
-
-        do! ctx.Response.OutputStream.WriteAsync(bytes, 0, bytes.Length)
-            |> Async.AwaitTask
-
-        ctx.Response.Close()
-    }
-
 let main startServer sendText update =
     let waitForPong =
         async {
@@ -65,3 +35,33 @@ let main startServer sendText update =
 
             do! sendText ctx "OK"
     }
+
+type t = private { ctx: HttpListenerContext }
+
+let startServer url =
+    let listener = new HttpListener()
+    listener.Prefixes.Add url
+    listener.Start()
+
+    async {
+        let! ctx = listener.GetContextAsync()
+        return { ctx = ctx }
+    }
+
+let sendText { ctx = ctx } (text: string) =
+    async {
+        let bytes = Encoding.UTF8.GetBytes text
+
+        do! ctx.Response.OutputStream.WriteAsync(bytes, 0, bytes.Length)
+            |> Async.AwaitTask
+
+        ctx.Response.Close()
+    }
+
+let updatePing state =
+    function
+    | HealthCheckRequested -> { healthCheckComplete = true }
+    | NewSubscriptionCreated _
+    | SubscriptionCreated _
+    | SubscriptionRemoved _
+    | SnapshotCreated _ -> state
