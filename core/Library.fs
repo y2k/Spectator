298b153c51ee1f821a4b6b1f4596adce5e4ef7df
y2k <itwisterlx@gmail.com>
2023-03-20 12:58:00 +0100

    refactoring

--- core/Library.fs
+++ core/Library.fs
@@ -1,40 +1,5 @@
 namespace Spectator.Core
 
-[<System.Obsolete>]
-type IReducer<'state, 'event> =
-    abstract member Invoke: ('state -> 'state * 'event list * 'result) -> 'result Async
-
-[<AutoOpen>]
-module Prelude =
-    module Async =
-        let delayAfter (timeout: System.TimeSpan) a =
-            async {
-                do! a
-                do! Async.Sleep timeout
-            }
-
-        let loopAll axs =
-            let loop a =
-                async {
-                    while true do
-                        match! a |> Async.catch with
-                        | Ok _ -> ()
-                        | Error e ->
-                            eprintfn "LOG: Error: %O" e
-                            exit -1
-                }
-
-            axs
-            |> List.map loop
-            |> Async.Parallel
-            |> Async.Ignore
-
-    module Result =
-        let toOption =
-            function
-            | Ok x -> Some x
-            | Error _ -> None
-
 module AsyncChannel =
     open System.Threading.Channels
 
@@ -45,5 +10,4 @@ module AsyncChannel =
     let write { chan = chan } x = chan.Writer.TryWrite x |> ignore
 
     let read { chan = chan } =
-        (chan.Reader.ReadAsync()).AsTask()
-        |> Async.AwaitTask
+        (chan.Reader.ReadAsync()).AsTask() |> Async.AwaitTask
