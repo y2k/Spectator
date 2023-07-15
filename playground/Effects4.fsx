398dc07d493241160cb140d124fce4aa51e586d5
y2k <itwisterlx@gmail.com>
2023-07-15 10:45:18 +0200

    update sample 4

--- playground/Effects4.fsx
+++ playground/Effects4.fsx
@@ -1,8 +1,46 @@
 #r "nuget: SodiumFRP.FSharp, 5.0.6"
 #r "nuget: FSharp.SystemTextJson, 1.1.23"
+#r "nuget: DiffPlex, 1.7.1"
 
-[<CompilerMessage("Incomplete hole", 130)>]
-let inline FIXME (x: _) = raise (exn $"Incomplete hole: {x}")
+module Prelude =
+    open System
+    open System.Text.Json
+    open System.Text.Json.Serialization
+
+    [<CompilerMessage("Incomplete hole", 130)>]
+    let inline FIXME (x: _) = raise (exn $"Incomplete hole: {x}")
+
+    module private Differ =
+        open DiffPlex.DiffBuilder
+        open DiffPlex.DiffBuilder.Model
+
+        let diff originalText modifiedText =
+            let diffResult =
+                InlineDiffBuilder.Instance.BuildDiffModel(originalText, modifiedText)
+
+            let printLine (line: DiffPiece) =
+                match line.Type with
+                | ChangeType.Inserted -> sprintf "+ %s" line.Text
+                | ChangeType.Deleted -> sprintf "- %s" line.Text
+                | ChangeType.Unchanged -> sprintf "  %s" line.Text
+                | x -> failwithf "%O" x
+
+            diffResult.Lines |> Seq.map printLine |> Seq.reduce (sprintf "%O\n%O")
+
+    let assertEffect cmdLog expectedEnc =
+        let expected =
+            expectedEnc |> Convert.FromBase64String |> Text.Encoding.UTF8.GetString
+
+        let options = JsonSerializerOptions(WriteIndented = true)
+        JsonFSharpOptions.Default().AddToJsonSerializerOptions(options)
+        let actual = JsonSerializer.Serialize(cmdLog, options)
+
+        if expected <> actual then
+            let diff = Differ.diff expected actual
+            let encoded = actual |> Text.Encoding.UTF8.GetBytes |> Convert.ToBase64String
+            failwithf "\n------------\n%s\n------------\n%s\n------------" diff encoded
+
+open Prelude
 
 type World = unit
 
@@ -17,12 +55,12 @@ let merge (fs: obj list) : Effect =
       name = "Merge"
       action = fun _ -> FIXME "" }
 
-// let dispatch (msg: _) =
-//     { param = box msg
-//       name = "Dispatch"
-//       action = fun _ -> FIXME "" }
-
 type Dispatch = Dispatch of param: obj
+
+let asEffect (data: obj) : Effect =
+    { name = "asEffect"
+      param = data
+      action = ignore }
 
 module TelegramApi =
     let sendMessage (user: string) (message: string) =
@@ -30,70 +68,172 @@ module TelegramApi =
           name = "TelegramSendMessage"
           action = fun _ -> FIXME "" }
 
+module HttpApi =
+    type DownloadFx = DownloadFx of url: string * callback: (byte[] -> Effect)
+
+module Id =
+    type t = private Id of string
+
+    let create (xs: string list) : t =
+        xs |> List.reduce (sprintf "%s:%s") |> Id
+
 module Global =
-    type NewSubscriptionCreated = NewSubscriptionCreated of user: string * url: string
+    type ProviderId = Rss
+
+    type GlobalEvent =
+        | NewSubscriptionCreated of user: string * url: string * id: Id.t
+        | NewSubscriptionRemoved of user: string * id: Id.t
+        | SubscriptionCreated of user: string * url: string * provider: ProviderId
+
+    type NewSnapshotCreated = NewSnapshotCreated of user: string * url: string
+
+(* Logic *)
 
 module Bot =
     open Global
 
-    type State = { subs: Map<string, string list> }
-    let emtpy = { subs = Map.empty }
+    type State =
+        { newSubs: Map<string, (string * Id.t) list>
+          subs: Map<string, (string * ProviderId) list> }
 
-    let handleState (NewSubscriptionCreated(user, url)) (state: State) : State =
-        { state with
-            subs =
-                state.subs
-                |> Map.tryFind user
-                |> Option.defaultValue []
-                |> fun xs -> url :: xs
-                |> fun xs -> Map.add user xs state.subs }
+    let empty =
+        { newSubs = Map.empty
+          subs = Map.empty }
 
-    let handle (state: State) (user, (message: string)) : obj =
+    let private updateLocal state user fsubs fupdate f =
+        fsubs state
+        |> Map.tryFind user
+        |> Option.defaultValue []
+        |> fun xs -> f xs
+        |> fun xs -> Map.add user xs (fsubs state)
+        |> fupdate state
+
+    let update e (state: State) : State =
+        match e with
+        | SubscriptionCreated(user, url, pId) ->
+            updateLocal state user (fun x -> x.subs) (fun s x -> { s with subs = x }) (fun xs -> (url, pId) :: xs)
+        | NewSubscriptionCreated(user, url, id) ->
+            updateLocal state user (fun x -> x.newSubs) (fun s x -> { s with newSubs = x }) (fun xs -> (url, id) :: xs)
+        | NewSubscriptionRemoved(user, newId) ->
+            updateLocal
+                state
+                user
+                (fun x -> x.newSubs)
+                (fun s x -> { s with newSubs = x })
+                (List.filter (fun (_, id) -> id <> newId))
+
+    let handle (user, (message: string)) (state: State) : obj =
         match message.Split ' ' with
         | [| "/ls" |] ->
-            state.subs
-            |> Map.tryFind user
-            |> Option.defaultValue []
-            |> List.fold (fun s x -> $"{s}\n- {x}") "Your subs:"
+            [ state.subs
+              |> Map.tryFind user
+              |> Option.defaultValue []
+              |> List.map (fun (url, provider) -> $"-[{provider}] {url}")
+              state.newSubs
+              |> Map.tryFind user
+              |> Option.defaultValue []
+              |> List.map (fun (url, _) -> $"-[In Progress] {url}") ]
+            |> List.concat
+            |> List.fold (fun s x -> $"{s}\n{x}") "Your subs:"
             |> TelegramApi.sendMessage user
             |> box
         | [| "/add"; url |] ->
             merge
-                [ Dispatch(NewSubscriptionCreated(user, url))
+                [ Dispatch(NewSubscriptionCreated(user, url, Id.create [ user; url ]))
                   TelegramApi.sendMessage user "Subscription created" ]
-        | _ -> TelegramApi.sendMessage user "[HELP message]"
+        | _ -> TelegramApi.sendMessage user "Unknown command. Enter /start to show help."
+
+module SubWorker =
+    open Global
+    open HttpApi
+
+    type State = private { newSubs: Id.t Set }
+    let empty = { newSubs = Set.empty }
+
+    let update e (s: State) =
+        match e with
+        | NewSubscriptionCreated(_, _, id) ->
+            { s with
+                newSubs = s.newSubs |> Set.add id }
+        | NewSubscriptionRemoved(_, id) ->
+            { s with
+                newSubs = s.newSubs |> Set.remove id }
+        | _ -> s
+
+    type DownloadCompleted = DownloadCompleted of id: Id.t * user: string * url: string * data: byte array
+
+    let downloadCompleted (DownloadCompleted(newSubId, user, url, (data: byte array))) (_: State) : Effect =
+        let isRss: bool = true
+
+        let e1 =
+            if isRss then
+                Dispatch(SubscriptionCreated(user, url, ProviderId.Rss)) |> asEffect
+            else
+                merge []
+
+        merge [ e1; Dispatch(NewSubscriptionRemoved(user, newSubId)) ]
+
+    let handle e : obj =
+        match e with
+        | NewSubscriptionCreated(user, url, id) ->
+            DownloadFx(url, (fun data -> Dispatch(DownloadCompleted(id, user, url, data)) |> asEffect))
+        | _ -> merge []
+
+module Notifications =
+    open Global
+
+    let handle (NewSnapshotCreated(user, url)) =
+        TelegramApi.sendMessage user $"New Snapshot: {url}"
 
 open Sodium.Frp
 open Global
-open System.Text.Json
-open System.Text.Json.Serialization
 
 let () =
-    let telegramMessageProducer = StreamSink.create ()
-
-    let newSubCreatedProducer: Global.NewSubscriptionCreated StreamSink =
-        StreamSink.create ()
-
-    let state =
-        [ newSubCreatedProducer |> Stream.map Bot.handleState ]
-        |> Stream.mergeAll (fun _ _ -> failwith "")
-        |> Stream.accum Bot.emtpy (<|)
-
     let clearLog = StreamSink.create ()
+    let telegramMessageProducer = StreamSink.create ()
+    let globalEventProducer = StreamSink.create ()
+    let subWorkerDownloadCompletedProducer = StreamSink.create ()
+    let newSnapCreated = StreamSink.create ()
+
+    let botState =
+        [ globalEventProducer |> Stream.map Bot.update ]
+        |> Stream.mergeAll (failwithf "%O")
+        |> Stream.accum Bot.empty (<|)
+
+    let subState =
+        [ globalEventProducer |> Stream.map SubWorker.update ]
+        |> Stream.mergeAll (failwithf "%O")
+        |> Stream.accum SubWorker.empty (<|)
 
     let effects =
         [ telegramMessageProducer
-          |> Stream.snapshot state (fun e s -> Bot.handle s e)
-          |> Stream.map (fun x -> [ x ])
-          clearLog |> Stream.map (fun _ -> []) ]
-        |> Stream.mergeAll (fun _ _ -> failwith "")
-        |> Stream.accum [] (fun x _ -> x)
+          |> Stream.snapshot botState Bot.handle
+          |> Stream.map (fun x -> Some x)
+
+          newSnapCreated
+          |> Stream.map Notifications.handle
+          |> Stream.map (fun x -> Some x)
+
+          globalEventProducer
+          |> Stream.snapshot botState (fun e _ -> SubWorker.handle e)
+          |> Stream.map (fun x -> Some x)
+          subWorkerDownloadCompletedProducer
+          |> Stream.snapshot subState SubWorker.downloadCompleted
+          |> Stream.map (fun x -> Some x)
+
+          clearLog |> Stream.map (fun _ -> None) ]
+        |> Stream.mergeAll (failwithf "%O")
+        |> Stream.accum [] (fun e effs ->
+            match e with
+            | Some e -> effs @ [ e ]
+            | None -> [])
+
+    (*  *)
 
     let mutable cmdLog = []
 
     let send msg target =
         StreamSink.send msg target
-        let effs = Cell.sample effects
 
         let rec exec (effs: obj list) =
             effs
@@ -101,34 +241,38 @@ let () =
                 match e with
                 | :? Dispatch as Dispatch msg ->
                     match msg with
-                    | :? NewSubscriptionCreated as x ->
-                        Transaction.post (fun _ -> StreamSink.send x newSubCreatedProducer)
-                    | _ -> FIXME ""
+                    | :? GlobalEvent as x -> Transaction.post (fun _ -> StreamSink.send x globalEventProducer)
+                    | m -> failwithf "Unknown message %O" m
                 | :? Effect as e ->
                     match e.name with
                     | "Merge" ->
-                        let params: obj list = unbox e.param
-                        exec params
-                    | _ -> ()
-                | _ -> ())
+                        let effects: obj list = unbox e.param
+                        exec effects
+                    | "TelegramSendMessage" -> ()
+                    | "asEffect" -> exec [ e.param ]
+                    | e -> failwithf "Unknown effect %O" e
+                | e -> failwithf "Unknown effect %O" e)
 
-        exec effs
-
-        let options = JsonSerializerOptions(WriteIndented = true)
-        JsonFSharpOptions.Default().AddToJsonSerializerOptions(options)
-        printfn "----------------\n%O" (JsonSerializer.Serialize(effs, options))
-
-        cmdLog <- cmdLog @ effs
+        exec (Cell.sample effects)
+        cmdLog <- cmdLog @ Cell.sample effects
         StreamSink.send () clearLog
+
+    (*  *)
 
     send ("y2k", "/ls") telegramMessageProducer
     send ("y2k", "/add https://g.com/") telegramMessageProducer
-    // send (NewSubscriptionCreated("y2k", "https://g.com/")) newSubCreatedProducer
     send ("y2k", "/ls") telegramMessageProducer
 
-    let options = JsonSerializerOptions(WriteIndented = false)
-    JsonFSharpOptions.Default().AddToJsonSerializerOptions(options)
-    let actual = JsonSerializer.Serialize(cmdLog, options)
+    send
+        (SubWorker.DownloadCompleted(Id.create [ "y2k"; "https://g.com/" ], "y2k", "https://g.com/", [||]))
+        subWorkerDownloadCompletedProducer
 
-    if """[{"name":"TelegramSendMessage","param":["y2k","Your subs:"]},{"name":"Merge","param":[{"Case":"NewSubscriptionCreated","Fields":["y2k","https://g.com/"]},{"name":"TelegramSendMessage","param":["y2k","Subscription created"]}]},{"name":"TelegramSendMessage","param":["y2k","Your subs:\n- https://g.com/"]}]""" <> actual then
-        failwithf "\n------------\n%s\n------------" actual
+    send ("y2k", "/ls") telegramMessageProducer
+
+    send (NewSnapshotCreated("y2k", "https://g.com/1")) newSnapCreated
+
+    (*  *)
+
+    assertEffect
+        cmdLog
+        """WwogIHsKICAgICJuYW1lIjogIlRlbGVncmFtU2VuZE1lc3NhZ2UiLAogICAgInBhcmFtIjogWwogICAgICAieTJrIiwKICAgICAgIllvdXIgc3ViczoiCiAgICBdCiAgfSwKICB7CiAgICAibmFtZSI6ICJNZXJnZSIsCiAgICAicGFyYW0iOiBbCiAgICAgIHsKICAgICAgICAiQ2FzZSI6ICJOZXdTdWJzY3JpcHRpb25DcmVhdGVkIiwKICAgICAgICAiRmllbGRzIjogWwogICAgICAgICAgInkyayIsCiAgICAgICAgICAiaHR0cHM6Ly9nLmNvbS8iLAogICAgICAgICAgInkyazpodHRwczovL2cuY29tLyIKICAgICAgICBdCiAgICAgIH0sCiAgICAgIHsKICAgICAgICAibmFtZSI6ICJUZWxlZ3JhbVNlbmRNZXNzYWdlIiwKICAgICAgICAicGFyYW0iOiBbCiAgICAgICAgICAieTJrIiwKICAgICAgICAgICJTdWJzY3JpcHRpb24gY3JlYXRlZCIKICAgICAgICBdCiAgICAgIH0KICAgIF0KICB9LAogIHsKICAgICJDYXNlIjogIkRvd25sb2FkRngiLAogICAgIkZpZWxkcyI6IFsKICAgICAgImh0dHBzOi8vZy5jb20vIiwKICAgICAge30KICAgIF0KICB9LAogIHsKICAgICJuYW1lIjogIlRlbGVncmFtU2VuZE1lc3NhZ2UiLAogICAgInBhcmFtIjogWwogICAgICAieTJrIiwKICAgICAgIllvdXIgc3Viczpcbi1bSW4gUHJvZ3Jlc3NdIGh0dHBzOi8vZy5jb20vIgogICAgXQogIH0sCiAgewogICAgIm5hbWUiOiAiTWVyZ2UiLAogICAgInBhcmFtIjogWwogICAgICB7CiAgICAgICAgIm5hbWUiOiAiYXNFZmZlY3QiLAogICAgICAgICJwYXJhbSI6IHsKICAgICAgICAgICJDYXNlIjogIlN1YnNjcmlwdGlvbkNyZWF0ZWQiLAogICAgICAgICAgIkZpZWxkcyI6IFsKICAgICAgICAgICAgInkyayIsCiAgICAgICAgICAgICJodHRwczovL2cuY29tLyIsCiAgICAgICAgICAgIHsKICAgICAgICAgICAgICAiQ2FzZSI6ICJSc3MiCiAgICAgICAgICAgIH0KICAgICAgICAgIF0KICAgICAgICB9CiAgICAgIH0sCiAgICAgIHsKICAgICAgICAiQ2FzZSI6ICJOZXdTdWJzY3JpcHRpb25SZW1vdmVkIiwKICAgICAgICAiRmllbGRzIjogWwogICAgICAgICAgInkyayIsCiAgICAgICAgICAieTJrOmh0dHBzOi8vZy5jb20vIgogICAgICAgIF0KICAgICAgfQogICAgXQogIH0sCiAgewogICAgIm5hbWUiOiAiTWVyZ2UiLAogICAgInBhcmFtIjogW10KICB9LAogIHsKICAgICJuYW1lIjogIk1lcmdlIiwKICAgICJwYXJhbSI6IFtdCiAgfSwKICB7CiAgICAibmFtZSI6ICJUZWxlZ3JhbVNlbmRNZXNzYWdlIiwKICAgICJwYXJhbSI6IFsKICAgICAgInkyayIsCiAgICAgICJZb3VyIHN1YnM6XG4tW1Jzc10gaHR0cHM6Ly9nLmNvbS8iCiAgICBdCiAgfSwKICB7CiAgICAibmFtZSI6ICJUZWxlZ3JhbVNlbmRNZXNzYWdlIiwKICAgICJwYXJhbSI6IFsKICAgICAgInkyayIsCiAgICAgICJOZXcgU25hcHNob3Q6IGh0dHBzOi8vZy5jb20vMSIKICAgIF0KICB9Cl0="""
